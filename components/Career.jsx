087ae416bbc5b3c5caa91ecac784671db5b004b6
witch342 <hide.admi@gmail.com>
2022-01-21 00:30:24 +0900

    ボタンの色の変更 Fixes #2

--- components/Career.jsx
+++ components/Career.jsx
@@ -3,7 +3,7 @@ import { Button, ButtonGroup } from '@chakra-ui/react'
 export default function Career() {
   return (
     <div>
-      <Button colorScheme='gray'>Career</Button>
+      <Button colorScheme='red'>Career</Button>
     </div>
   )
 }
