55d3abcbaa7b4bad4fcd134b7e9f07b8cfdf56d2
witch342 <hide.admi@gmail.com>
2022-01-23 23:07:31 +0900

    記述を削除

--- components/Career.jsx
+++ components/Career.jsx
@@ -1,9 +1,6 @@
-import { Box, Button, ButtonGroup } from '@chakra-ui/react'
 
 export default function Career() {
   return (
-    <Box>
-      <Button colorScheme='gray'>Career</Button>
-    </Box>
+    
   )
 }
